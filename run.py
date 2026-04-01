85648cb4e1c0838619c77fa356bda4f69053801c
Federico Verstraeten Portomeñe <federico.verstraeten@gmail.com>
2026-04-01 11:36:43 -0300

    Refine autoalignment screen with full controls and guidance UI

--- run.py
+++ run.py
@@ -89,6 +89,21 @@ def homepage():
   # run html template
   return render_template('alignment.html', context=context)
 
+
+@app.route("/autoalignment")
+def autoalignment_mode():
+
+  # empty plots
+  plot_lidar_signal = plotly_plot.plotly_empty_signal("raw")
+  plot_lidar_range_correction = plotly_plot.plotly_empty_signal("rangecorrected")
+
+  context = {"plot_lidar_signal": plot_lidar_signal,
+             "plot_lidar_range_correction": plot_lidar_range_correction,
+             "globalconfig": globalconfig
+            }
+
+  return render_template('autoalignment.html', context=context)
+
 @app.route("/acquisition")
 def acquisition_mode():
 
